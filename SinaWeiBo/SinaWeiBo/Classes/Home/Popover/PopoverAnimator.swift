7dcc23ec288f564b94f10eaf950ebcdb96b3d049
caiiiac <caiiiac@163.com>
2017-04-22 11:31:03 +0800

    自定义弹窗动画

--- SinaWeiBo/SinaWeiBo/Classes/Home/Popover/PopoverAnimator.swift
+++ SinaWeiBo/SinaWeiBo/Classes/Home/Popover/PopoverAnimator.swift
@@ -10,4 +10,74 @@ import UIKit
 
 class PopoverAnimator: NSObject {
 
+    fileprivate var isPresented : Bool = false
+}
+
+//MARK: - 自定义转场代理 UIViewControllerTransitioningDelegate
+extension PopoverAnimator : UIViewControllerTransitioningDelegate {
+   
+    //改变弹出View尺寸
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        return SANPresentationController(presentedViewController: presented, presenting: presenting)
+    }
+    
+    //自定义弹出动画
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresented = true
+        return self
+    }
+    //自定义消失动画
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        isPresented = false
+        return self
+    }
+}
+
+//MARK: - 自定义转场代理 UIViewControllerTransitioningDelegate
+extension PopoverAnimator : UIViewControllerAnimatedTransitioning {
+    
+    // 动画执行时间
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    //获取转场上下文,可获取弹出和消失的View
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        isPresented ? animationForPresented(using: transitionContext) : animationForDismissed(using: transitionContext)
+    }
+    
+    //弹出动画
+    fileprivate func animationForPresented(using transitionContext: UIViewControllerContextTransitioning){
+        //获取弹出的View
+        let presentedView = transitionContext.view(forKey: .to)!
+        //将弹出的View添加到containerView中
+        transitionContext.containerView.addSubview(presentedView)
+        
+        //执行动画,设置View初始状态
+        presentedView.transform = CGAffineTransform(scaleX: 1.0, y: 0.0)
+        presentedView.layer.anchorPoint = CGPoint(x: 0.5, y: 0)
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            //View最终状态
+            presentedView.transform = CGAffineTransform.identity
+        }) { (_) in
+            //必须告诉转场上下文已经完成动画
+            transitionContext.completeTransition(true)
+        }
+        
+    }
+    //消失动画
+    fileprivate func animationForDismissed(using transitionContext: UIViewControllerContextTransitioning){
+        //获取消失的View
+        let dismissedView = transitionContext.view(forKey: .from)
+        
+        //执行动画
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: { 
+            dismissedView?.transform = CGAffineTransform(scaleX: 1.0, y: 0.0001)
+        }) { (_) in
+            dismissedView?.removeFromSuperview()
+            //告诉转场上下文已经完成动画
+            transitionContext.completeTransition(true)
+        }
+    }
 }
