bf5f5565b4d42677eb4c1d19a87c829af5ac1b04
caiiiac <caiiiac@163.com>
2017-04-26 16:43:42 +0800

    欢迎界面

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Welcome/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/4/26.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
