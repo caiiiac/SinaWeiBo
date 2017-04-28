ca22b6a3800c84a0cbcc0acd234ebfbad7f924e7
caiiiac <caiiiac@163.com>
2017-04-28 15:04:46 +0800

    user优化

--- SinaWeiBo/SinaWeiBo/Classes/Home/HomeModel/User.swift
+++ SinaWeiBo/SinaWeiBo/Classes/Home/HomeModel/User.swift
@@ -15,9 +15,33 @@ class User: NSObject {
     //昵称
     var screen_name : String?
     //认证类型
-    var verified_type : Int = -1
+    var verified_type : Int = -1 {
+        didSet {
+            switch verified_type {
+            case 0:
+                verifiedImage = UIImage(named: "avatar_vip")
+            case 2, 3, 5:
+                verifiedImage = UIImage(named: "avatar_enterprise_vip")
+            case 220:
+                verifiedImage = UIImage(named: "avatar_grassroot")
+            default:
+                verifiedImage = nil
+            }
+        }
+    }
     //会员等级
-    var mbrank : Int = 0
+    var mbrank : Int = 0 {
+        didSet {
+            if mbrank > 0 && mbrank <= 6 {
+                vipImage = UIImage(named: "common_icon_membership_level\(mbrank)")
+            }
+        }
+    }
+    
+    //计算属性
+    var verifiedImage : UIImage?
+    var vipImage : UIImage?
+    
     
     
     init(dict : [String : Any]) {
