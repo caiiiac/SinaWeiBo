873110fd7a316a258a2851177391fa3d6ffc9512
caiiiac <caiiiac@163.com>
2017-05-02 11:08:37 +0800

    add viewModel

---
+++ SinaWeiBo/SinaWeiBo/Classes/Home/HomeModel/StatusViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StatusViewModel.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/2.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class StatusViewModel: NSObject {
+
+}
