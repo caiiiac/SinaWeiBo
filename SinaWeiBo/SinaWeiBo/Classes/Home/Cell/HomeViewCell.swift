3898ad7fc14fa03b0780690b3b9222da8531c655
caiiiac <caiiiac@163.com>
2017-05-02 15:48:34 +0800

    add homeCell

---
+++ SinaWeiBo/SinaWeiBo/Classes/Home/Cell/HomeViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewCell.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/2.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
