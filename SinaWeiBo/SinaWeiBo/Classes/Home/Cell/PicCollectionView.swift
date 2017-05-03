7a3f439b41d54f6d0ed3f7c23bc3440a75031113
caiiiac <caiiiac@163.com>
2017-05-03 16:11:42 +0800

    add 配图cell

---
+++ SinaWeiBo/SinaWeiBo/Classes/Home/Cell/PicCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  PicCollectionView.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/3.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class PicCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
