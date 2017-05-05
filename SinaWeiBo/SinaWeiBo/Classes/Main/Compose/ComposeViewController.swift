ba97d8a4e00f5e9c4a86bf3627739d98c300c4be
caiiiac <caiiiac@163.com>
2017-05-05 21:40:51 +0800

    add 发布微博页

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Compose/ComposeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ComposeViewController.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/5.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
