b0687912585266d5b9ed90c3d2bb70b9b195da39
caiiiac <caiiiac@163.com>
2017-05-06 21:22:11 +0800

    底部工具栏

--- SinaWeiBo/SinaWeiBo/Classes/Main/Compose/ComposeViewController.swift
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Compose/ComposeViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ComposeViewController: UIViewController {
 
     @IBOutlet weak var composeTextView: ComposeTextView!
+    //工具栏约束
+    @IBOutlet weak var toolBarBottomCons: NSLayoutConstraint!
+    
     fileprivate lazy var titleView : ComposeTitleView = ComposeTitleView()
     
     override func viewDidLoad() {
@@ -19,6 +22,10 @@ class ComposeViewController: UIViewController {
         // Do any additional setup after loading the view.
         //设置导航
         setupNavigationBar()
+        
+        //监听键盘通知
+        NotificationCenter.default.addObserver(self, selector: #selector(ComposeViewController.keyboardWillChangeFrame(note:)), name: .UIKeyboardWillChangeFrame, object: nil)
+
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -26,10 +33,15 @@ class ComposeViewController: UIViewController {
         
         composeTextView.becomeFirstResponder()
     }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
 }
 //MARK: - UI
 extension ComposeViewController {
+    //设置导航
     fileprivate func setupNavigationBar() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: .plain, target: self, action: #selector(ComposeViewController.closeItemClick))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "发送", style: .plain, target: self, action: #selector(ComposeViewController.sendItemClick))
@@ -47,6 +59,26 @@ extension ComposeViewController {
         dismiss(animated: true, completion: nil)
     }
     @objc fileprivate func sendItemClick() {
+        
+    }
+    
+    @objc fileprivate func keyboardWillChangeFrame(note : NSNotification) {
+        //获取动画执行时间
+        let duration = note.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! TimeInterval
+     
+        //获取键盘最终Y值
+        let endFrame = (note.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let y = endFrame.origin.y
+        
+        //计算工具栏到底部的间距
+        let margin = UIScreen.main.bounds.height - y
+        
+        //动画
+        toolBarBottomCons.constant = margin
+        UIView.animate(withDuration: duration) { 
+            self.view.layoutIfNeeded()
+        }
+        
         
     }
 }
