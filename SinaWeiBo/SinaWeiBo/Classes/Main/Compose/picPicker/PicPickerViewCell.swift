23422e7f7e534a4321123fc0ed24f5d958fdc619
caiiiac <caiiiac@163.com>
2017-05-07 16:04:23 +0800

    add PicPickerViewCell

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Compose/picPicker/PicPickerViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PicPickerViewCell.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/7.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class PicPickerViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
