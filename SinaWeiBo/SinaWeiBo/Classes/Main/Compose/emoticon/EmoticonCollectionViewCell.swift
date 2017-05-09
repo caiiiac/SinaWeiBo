3b2099855e94f564c19135eecb15736edead3029
caiiiac <caiiiac@163.com>
2017-05-09 22:57:59 +0800

    add 表情cell

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Compose/emoticon/EmoticonCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EmoticonCollectionViewCell.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/9.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class EmoticonCollectionViewCell: UICollectionViewCell {
+    
+}
