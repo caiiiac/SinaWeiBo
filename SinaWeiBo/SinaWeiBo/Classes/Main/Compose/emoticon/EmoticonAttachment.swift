1eddba349388f19af504ddace39f843200dd2edb
caiiiac <caiiiac@163.com>
2017-05-11 23:08:47 +0800

    add textView-Extension

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Compose/emoticon/EmoticonAttachment.swift
@@ -0,0 +1,13 @@
+//
+//  EmoticonAttachment.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/11.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class EmoticonAttachment: NSTextAttachment {
+
+}
---
+++ SinaWeiBo/SinaWeiBo/Classes/Tools/Category/UITextView-Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UITextView-Extension.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/11.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import Foundation
