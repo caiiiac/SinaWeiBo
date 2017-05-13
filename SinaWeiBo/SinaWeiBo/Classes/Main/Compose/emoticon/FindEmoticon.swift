1e82ae93a71b83c8044fb8b0a86b88db6f00cef7
caiiiac <caiiiac@163.com>
2017-05-13 21:12:51 +0800

    add findEmoticon

---
+++ SinaWeiBo/SinaWeiBo/Classes/Main/Compose/emoticon/FindEmoticon.swift
@@ -0,0 +1,13 @@
+//
+//  FindEmoticon.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/13.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class FindEmoticon: NSObject {
+
+}
