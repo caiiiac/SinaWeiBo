12e70a61a3b141f537d2c61c5722766b1125cd1b
caiiiac <caiiiac@163.com>
2017-05-16 22:41:26 +0800

    add 浏览器cell

---
+++ SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/PhotoBrowserCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoBrowserCollectionViewCell.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/16.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class PhotoBrowserCollectionViewCell: UICollectionViewCell {
+    
+}
