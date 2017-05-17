4c0f79a58b2875b82c1c60faa0d54beb249ca9d8
caiiiac <caiiiac@163.com>
2017-05-17 22:41:06 +0800

    图片下载进度

--- SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/PhotoBrowserCollectionViewCell.swift
+++ SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/PhotoBrowserCollectionViewCell.swift
@@ -13,37 +13,14 @@ class PhotoBrowserCollectionViewCell: UICollectionViewCell {
     //MARK: - 属性
     var picURL : URL? {
         didSet {
-            //校验nil
-            guard let picUrl = picURL else {
-                return
-            }
-            //取出image
-            let image = SDWebImageManager.shared().imageCache?.imageFromDiskCache(forKey: picUrl.absoluteString)
-            
-            //计算imageView的Frame
-            
-            let width = UIScreen.main.bounds.width
-            let height = width / (image?.size.width)! * (image?.size.height)!
-            
-            
-            var y : CGFloat = 0
-            if height > UIScreen.main.bounds.height {
-                y = 0
-            } else {
-                y = (UIScreen.main.bounds.height - height) * 0.5
-            }
-            imageView.frame = CGRect(x: 0, y: y, width: width, height: height)
-            
-            //设置imageView的图片
-            imageView.image = image
-            
-            //设置contentSize
-            scrollView.contentSize = CGSize(width: 0, height: height)
+            setupContent(picURL: picURL)
         }
     }
     
     fileprivate lazy var scrollView : UIScrollView = UIScrollView()
     fileprivate lazy var imageView : UIImageView = UIImageView()
+    //下载进度View
+    fileprivate lazy var progressView : ProgressView = ProgressView()
     
     //MARK: - 构造函数
     override init(frame: CGRect) {
@@ -64,8 +41,65 @@ extension PhotoBrowserCollectionViewCell {
     fileprivate func setupUI() {
         //添加控件
         contentView.addSubview(scrollView)
-        contentView.addSubview(imageView)
+        contentView.addSubview(progressView)
+        scrollView.addSubview(imageView)
         
+        //frame
         scrollView.frame = contentView.bounds
+        progressView.bounds = CGRect(x: 0, y: 0, width: 50, height: 50)
+        progressView.center = contentView.center
+        
+        //进度View属性
+        progressView.isHidden = true
+        progressView.backgroundColor = UIColor.clear
+    }
+}
+
+extension PhotoBrowserCollectionViewCell {
+    //设置图片
+    fileprivate func setupContent(picURL : URL?) {
+        //校验nil
+        guard let picUrl = picURL else {
+            return
+        }
+        //取出image
+        let image = SDWebImageManager.shared().imageCache?.imageFromDiskCache(forKey: picUrl.absoluteString)
+        
+        //计算imageView的Frame
+        
+        let width = UIScreen.main.bounds.width
+        let height = width / (image?.size.width)! * (image?.size.height)!
+        
+        
+        var y : CGFloat = 0
+        if height > UIScreen.main.bounds.height {
+            y = 0
+        } else {
+            y = (UIScreen.main.bounds.height - height) * 0.5
+        }
+        imageView.frame = CGRect(x: 0, y: y, width: width, height: height)
+        
+        //下载图片显示进度view
+        progressView.isHidden = false
+        
+        //设置imageView的图片
+        imageView.sd_setImageWithPreviousCachedImage(with: getBigURL(smallURL: picUrl), placeholderImage: image, options: [], progress: { (current, total, _) in
+            //获取主线程刷新进度
+            DispatchQueue.main.async {
+                self.progressView.progress = CGFloat(current) / CGFloat(total)
+            }
+        }) { (_, _, _, _) in
+            self.progressView.isHidden = true
+        }
+        
+        //设置contentSize
+        scrollView.contentSize = CGSize(width: 0, height: height)
+    }
+    
+    //大图链接
+    fileprivate func getBigURL(smallURL : URL) -> URL {
+        let smallURLString = smallURL.absoluteString as NSString
+        let bigURLString = smallURLString.replacingOccurrences(of: "thumbnail", with: "bmiddle")
+        return URL(string: bigURLString)!
     }
 }
--- SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/ProgressView.swift
+++ SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/ProgressView.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class ProgressView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //MARK: - 属性
+    var progress : CGFloat = 0 {
+        didSet {
+            self.setNeedsDisplay()
+        }
     }
-    */
-
+    
+    //MARK: - 重写drawRect方法
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        //获取参数
+        let center = CGPoint(x: rect.width * 0.5, y: rect.height * 0.5)
+        let radius = rect.width * 0.5 - 3
+        let startAngle = -(CGFloat.pi / 2)
+        let endAngle = CGFloat.pi * 2 * progress + startAngle
+        
+        //创建贝塞尔曲线
+        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        //绘制到中心点的线
+        path.addLine(to: center)
+        path.close()
+        //设置绘制颜色
+        UIColor(white: 1.0, alpha: 0.4).setFill()
+        
+        //开始绘制
+        path.fill()
+        
+    }
+    
 }
