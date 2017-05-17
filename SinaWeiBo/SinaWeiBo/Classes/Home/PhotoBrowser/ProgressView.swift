5ac92bcb193b97800c6573efbcb429a3f459d953
caiiiac <caiiiac@163.com>
2017-05-17 22:40:21 +0800

    add progressView

---
+++ SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/ProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressView.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/17.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class ProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
