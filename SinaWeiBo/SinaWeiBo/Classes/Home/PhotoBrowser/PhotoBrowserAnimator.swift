8057954092507fda7a7d8311fff7adc19c6638b9
caiiiac <caiiiac@163.com>
2017-05-19 22:45:55 +0800

    add PhotoBrowserAnimator

---
+++ SinaWeiBo/SinaWeiBo/Classes/Home/PhotoBrowser/PhotoBrowserAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoBrowserAnimator.swift
+//  SinaWeiBo
+//
+//  Created by 唐三彩 on 2017/5/19.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import UIKit
+
+class PhotoBrowserAnimator: NSObject {
+
+}
